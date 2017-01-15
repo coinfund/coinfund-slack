4d190e8356a81a31c63a4461a4ab78420813a2be
Jake Brukhman <jbrukh@gmail.com>
2017-01-14 21:04:38 -0500

    First commit.

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('coinfundSlack', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
