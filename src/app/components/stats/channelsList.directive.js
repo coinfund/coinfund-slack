b6aee10191dcec4e53a04dcf5fa84162ee772234
Jake Brukhman <jbrukh@gmail.com>
2017-01-22 14:54:12 -0500

    Bugfixes

--- src/app/components/stats/channelsList.directive.js
+++ src/app/components/stats/channelsList.directive.js
@@ -2,7 +2,7 @@
 * @Author: Jake Brukhman
 * @Date:   2017-01-14 21:30:22
 * @Last Modified by:   Jake Brukhman
-* @Last Modified time: 2017-01-22 14:39:41
+* @Last Modified time: 2017-01-22 14:54:04
 */
 
 (function() {
@@ -13,15 +13,14 @@
     .directive('channelsList', channelsList);
 
   /** @ngInject */
-  function channelsList($log) {
+  function channelsList() {
     var directive = {
       restrict: 'EA',
       templateUrl: 'app/components/stats/channelsList.html',
       scope: {
         channelsData: '='
       },
-      link: function(scope) {
-
+      link: function() {
       }
     };
     return directive;
